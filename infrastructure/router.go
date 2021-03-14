d4858ba68a9e77fff80cf81351ececc5fb58cc6b
Kei Yonekawa <kei.yonekawa0614@gmail.com>
2021-03-14 20:29:31 +0900

    add: infrastructure package

---
+++ infrastructure/router.go
@@ -0,0 +1,20 @@
+package infrastructure
+
+import (
+	"net/http"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func Init() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello wold!!")
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
--- server.go
+++ server.go
@@ -1,14 +1,7 @@
 package main
 
-import (
-	"net/http"
-    "github.com/labstack/echo"
-)
+import "app/infrastructure"
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":8080"))
+    infrastructure.Init()
 }
