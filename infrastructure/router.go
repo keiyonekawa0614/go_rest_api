fe93164205a0fadbfbbba68b8a29294fda794032
Kei Yonekawa <kei.yonekawa0614@gmail.com>
2021-03-15 21:18:01 +0900

    fix: routing

--- infrastructure/router.go
+++ infrastructure/router.go
@@ -12,9 +12,11 @@ func Init() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello wold!!")
-	})
+	e.GET("/users", func(c echo.Context) error { return c.String(http.StatusOK, "GetUsers") })
+	e.GET("/users/:id", func(c echo.Context) error { return c.String(http.StatusOK, "GetUser") })
+	e.POST("/users", func(c echo.Context) error { return c.String(http.StatusOK, "CreateUser") })
+	e.PUT("/users/:id", func(c echo.Context) error { return c.String(http.StatusOK, "UpdateUser") })
+	e.DELETE("/users/:id", func(c echo.Context) error { return c.String(http.StatusOK, "DeleteUser") })
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
